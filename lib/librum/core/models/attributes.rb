682ad5aacab155e8d0062c5d6fd0a01e07e6ec97
Rob Smith <merlin@sleepingkingstudios.com>
2023-05-26 01:35:23 -0400

    Implement model commands for slug attribute.

---
+++ lib/librum/core/models/attributes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'librum/core/models'
+
+module Librum::Core::Models
+  # Commands for generating and managing model attributes.
+  module Attributes; end
+end
---
+++ lib/librum/core/models/queries.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'librum/core/models'
+
+module Librum::Core::Models
+  # Commands for querying model collections.
+  module Queries; end
+end
