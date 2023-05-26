5865ba95e8126d8351e2d74b62bf35b7bd745a1b
Rob Smith <merlin@sleepingkingstudios.com>
2023-05-26 17:23:37 -0400

    Implement ApiController.

---
+++ app/controllers/librum/core/api_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'cuprum/rails/controller'
+
+require 'librum/core/responders/json_responder'
+require 'librum/core/serializers/json'
+
+module Librum::Core
+  # Abstract base class for API controllers.
+  class ApiController < Librum::Core::ApplicationController
+    include Cuprum::Rails::Controller
+
+    protect_from_forgery with: :null_session
+
+    def self.repository
+      return @repository if @repository
+
+      @repository = Cuprum::Rails::Repository.new
+    end
+
+    def self.serializers
+      Librum::Core::Serializers::Json.default_serializers
+    end
+
+    default_format :json
+
+    responder :json, Librum::Core::Responders::JsonResponder
+  end
+end
