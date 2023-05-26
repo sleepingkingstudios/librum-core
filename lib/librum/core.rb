fa122edb238fe42f512ff5c7aa201e0ab642bb33
Rob Smith <merlin@sleepingkingstudios.com>
2023-05-26 13:49:58 -0400

    Refactor engine configuration.

--- lib/librum/core.rb
+++ lib/librum/core.rb
@@ -7,5 +7,23 @@ require 'librum/core/railtie' if defined?(Rails::Railtie)
 # The Ruby tabletop campaign companion.
 module Librum
   # Librum engine defining shared functionality and core models.
-  module Core; end
+  module Core
+    class << self
+      # @return [Class, nil] the base authentication error.
+      def authentication_error
+        configuration[:authentication_error]&.constantize
+      end
+
+      # @param error_class [String] the class name of the base authentication
+      #   error.
+      def authentication_error=(error_class)
+        configuration[:authentication_error] = error_class
+      end
+
+      # @return [Hash{Symbol=>Object}] the engine configuration.
+      def configuration
+        @configuration ||= {}
+      end
+    end
+  end
 end
--- lib/librum/core/responders/json_responder.rb
+++ lib/librum/core/responders/json_responder.rb
@@ -12,10 +12,6 @@ require 'librum/core/responders'
 module Librum::Core::Responders
   # Base class for generating JSON responses.
   class JsonResponder < Cuprum::Rails::Responders::JsonResponder
-    BASE_AUTHENTICATION_ERROR =
-      Rails.application.config.librum_core.authentication_error.constantize
-    private_constant :BASE_AUTHENTICATION_ERROR
-
     match :failure, error: Cuprum::Collections::Errors::FailedValidation \
     do |result|
       render_failure(result.error, status: 422)
@@ -33,7 +29,7 @@ module Librum::Core::Responders
       render_failure(result.error, status: 400)
     end
 
-    match :failure, error: BASE_AUTHENTICATION_ERROR do |result|
+    match :failure, error: Librum::Core.authentication_error do |result|
       render_failure(
         Rails.env.development? ? result.error : authentication_error,
         status: 401
