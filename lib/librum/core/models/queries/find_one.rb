32d82dddd673d295d042581e86fa59ef56f836af
Rob Smith <sleepingkingstudios@gmail.com>
2024-03-08 19:42:31 -0500

    Implement Models::Queries::FindOne.

---
+++ lib/librum/core/models/queries/find_one.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'cuprum/command'
+
+module Librum::Core::Models::Queries
+  # Query command to find an entity by its id or slug value.
+  class FindOne < Cuprum::Command
+    UUID_PATTERN = /\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/
+    private_constant :UUID_PATTERN
+
+    # @param collection [Object] The collection to query.
+    def initialize(collection:)
+      super()
+
+      @collection = collection
+    end
+
+    # @return [Object] the collection to query.
+    attr_reader :collection
+
+    private
+
+    def process(value:)
+      if value.match?(UUID_PATTERN)
+        collection.find_one.call(primary_key: value)
+      else
+        Librum::Core::Models::Queries::FindBySlug
+          .new(collection: collection)
+          .call(slug: value)
+      end
+    end
+  end
+end
