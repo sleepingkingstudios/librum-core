08f75bdb4217c1b8b5a73336a8a5ee57c649b03e
Rob Smith <sleepingkingstudios@gmail.com>
2025-03-07 16:10:22 -0500

    Update minimum Rails version to 7.2.

--- spec/support/migrate/20231108210958_create_projects.rb
+++ spec/support/migrate/20231108210958_create_projects.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateProjects < ActiveRecord::Migration[7.1]
+class CreateProjects < ActiveRecord::Migration[7.2]
   def change
     create_table :projects, id: :uuid do |t|
       t.string :name
