423a2f885b5c5328b753a2e943bf83c9b576c241
Rob Smith <sleepingkingstudios@gmail.com>
2025-05-10 01:46:13 -0400

    Implement Commands::Resources::New.

---
+++ lib/librum/core/commands/resources/new.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'cuprum/rails/commands/resources/new'
+
+module Librum::Core::Commands::Resources
+  # Builds an entity with generated uuid ID and slug.
+  class New < Cuprum::Rails::Commands::Resources::New
+    ATTRIBUTE_NAMES_FOR_SLUG = %i[name].freeze
+    private_constant :ATTRIBUTE_NAMES_FOR_SLUG
+
+    private
+
+    def attribute_names_for_slug
+      ATTRIBUTE_NAMES_FOR_SLUG
+    end
+
+    def build_entity(attributes:)
+      attributes = attributes.merge(
+        'id'   => step { generate_uuid(attributes) },
+        'slug' => step { generate_slug(attributes) }
+      )
+
+      super
+    end
+
+    def generate_slug(attributes)
+      return attributes['slug'] if attributes.key?('slug')
+
+      Librum::Core::Commands::Attributes::GenerateSlug
+        .new(attribute_names: attribute_names_for_slug)
+        .call(attributes:)
+    end
+
+    def generate_uuid(attributes)
+      return attributes['id'] if attributes['id'].present?
+
+      SecureRandom.uuid_v7
+    end
+  end
+end
--- spec/support/examples/commands/users_examples.rb
+++ spec/support/examples/commands/users_examples.rb
@@ -12,18 +12,18 @@ module Spec::Support::Examples::Commands
     # Sample data for User objects.
     USERS_FIXTURES = [
       {
-        'id'       => '0196b857-8f4b-7904-b4db-5c6b74213a99',
-        'name'     => 'Alan Bradley',
-        'slug'     => 'alan-bradley',
-        'role'     => 'user',
-        'password' => 'tronlives'
-      }.freeze,
-      {
         'id'       => '0196b858-6ba3-77f7-8194-e57ebeb5e615',
         'name'     => 'Kevin Flynn',
         'slug'     => 'kevin-flynn',
         'role'     => 'user',
         'password' => 'theperfectsystem'
+      }.freeze,
+      {
+        'id'       => '0196b857-8f4b-7904-b4db-5c6b74213a99',
+        'name'     => 'Alan Bradley',
+        'slug'     => 'alan-bradley',
+        'role'     => 'user',
+        'password' => 'tronlives'
       }.freeze,
       {
         'id'       => '0196b883-000d-72b8-a6c3-2b86c784a634',
@@ -40,5 +40,52 @@ module Spec::Support::Examples::Commands
         'password' => 'mastercontrol'
       }.freeze
     ].freeze
+
+    deferred_context 'with parameters for a User command' do
+      let(:repository) { Cuprum::Collections::Basic::Repository.new }
+      let(:resource) do
+        Cuprum::Rails::Resource.new(name: 'users', **resource_options)
+      end
+      let(:resource_options) do
+        {
+          default_order:        'id',
+          permitted_attributes:,
+          primary_key_name:     'id'
+        }
+      end
+      let(:permitted_attributes) do
+        %w[name password role slug]
+      end
+      let(:collection_options) { { primary_key_type: String } }
+      let(:default_contract) do
+        Stannum::Contracts::HashContract.new(allow_extra_keys: true) do
+          key 'name', Stannum::Constraints::Presence.new
+          key 'role', Stannum::Constraints::Presence.new
+        end
+      end
+      let(:fixtures_data) { USERS_FIXTURES }
+
+      ##########################################################################
+      ###                         Resource Parameters                        ###
+      ##########################################################################
+
+      let(:extra_attributes) do
+        {
+          'derezzed' => false
+        }
+      end
+      let(:invalid_attributes) do
+        {
+          'name' => 'CLU',
+          'role' => nil
+        }
+      end
+      let(:valid_attributes) do
+        {
+          'name' => 'CLU',
+          'role' => 'program'
+        }
+      end
+    end
   end
 end
