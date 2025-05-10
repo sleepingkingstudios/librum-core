7327ca5cd44aa2d2748bab3cdc6ef56b7f75289b
Rob Smith <sleepingkingstudios@gmail.com>
2025-05-10 01:48:09 -0400

    Implement Commands::Resources::Create.

---
+++ spec/librum/core/commands/resources/create_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+require 'cuprum/collections/rspec/fixtures'
+require 'cuprum/rails/rspec/deferred/commands/resources/create_examples'
+
+require 'support/examples/commands/users_examples'
+
+RSpec.describe Librum::Core::Commands::Resources::Create do
+  include Cuprum::Rails::RSpec::Deferred::Commands::Resources::CreateExamples
+  include Spec::Support::Examples::Commands::UsersExamples
+
+  subject(:command) { described_class.new(repository:, resource:) }
+
+  let(:expected_slug) do
+    matched_attributes.fetch('slug') do
+      Librum::Core::Commands::Attributes::GenerateSlug
+        .new
+        .call(attributes: matched_attributes)
+        .value
+    end
+  end
+  let(:expected_attributes) do
+    empty_attributes
+      .merge(tools.hash_tools.convert_keys_to_strings(matched_attributes))
+      .merge(
+        'id'   => be_a_uuid,
+        'slug' => expected_slug
+      )
+  end
+
+  include_deferred 'with parameters for a User command'
+
+  include_deferred 'should implement the Create command',
+    default_contract: true \
+  do
+    describe 'with id: nil' do
+      let(:matched_attributes) do
+        configured_valid_attributes.merge('id' => nil)
+      end
+      let(:permitted_attributes) do
+        super() << 'id'
+      end
+
+      include_deferred 'should create the entity'
+    end
+
+    describe 'with id: an empty String' do
+      let(:matched_attributes) do
+        configured_valid_attributes.merge('id' => '')
+      end
+      let(:permitted_attributes) do
+        super() << 'id'
+      end
+
+      include_deferred 'should create the entity'
+    end
+
+    describe 'with id: value' do
+      let(:id) { '00000000-0000-0000-0000-000000000000' }
+      let(:matched_attributes) do
+        configured_valid_attributes.merge('id' => id)
+      end
+      let(:expected_attributes) do
+        super().merge('id' => id)
+      end
+      let(:permitted_attributes) do
+        super() << 'id'
+      end
+
+      include_deferred 'should create the entity'
+    end
+
+    describe 'with slug: nil' do
+      let(:matched_attributes) do
+        configured_valid_attributes.merge('slug' => nil)
+      end
+
+      include_deferred 'should create the entity'
+    end
+
+    describe 'with slug: an empty String' do
+      let(:matched_attributes) do
+        configured_valid_attributes.merge('slug' => '')
+      end
+
+      include_deferred 'should create the entity'
+    end
+
+    describe 'with slug: value' do
+      let(:slug) { 'custom-slug' }
+      let(:matched_attributes) do
+        configured_valid_attributes.merge('slug' => slug)
+      end
+      let(:expected_attributes) do
+        super().merge('slug' => slug)
+      end
+
+      include_deferred 'should create the entity'
+    end
+  end
+end
