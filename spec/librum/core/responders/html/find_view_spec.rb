ad9102599e1f8758d49679b1dbe4af7b8c3063b4
Rob Smith <sleepingkingstudios@gmail.com>
2025-09-08 13:04:25 -0400

    Implement FindView#view_paths.
    - Add support for defining views in shared components.

--- spec/librum/core/responders/html/find_view_spec.rb
+++ spec/librum/core/responders/html/find_view_spec.rb
@@ -59,6 +59,18 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
     end
   end
 
+  deferred_context 'when the provider defines components' do
+    example_constant 'Spec::Components', Module.new
+
+    before(:example) do
+      stub_provider(
+        Librum::Components.provider,
+        :components,
+        Spec::Components
+      )
+    end
+  end
+
   describe '.new' do
     it 'should define the constructor' do
       expect(described_class)
@@ -114,6 +126,17 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
         it 'returns the component defined by the application' do
           expect(component).to be View::Books::Publish
         end
+
+        wrap_deferred 'when the provider defines components' do
+          context 'when the provider defines the matching component' do # rubocop:disable RSpec/NestedGroups
+            example_class 'Spec::Components::Views::Books::Publish',
+              ViewComponent::Base
+
+            it 'returns the component defined by the application' do
+              expect(component).to be View::Books::Publish
+            end
+          end
+        end
       end
     end
 
@@ -139,6 +162,28 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
         it 'returns the component defined by the application' do
           expect(component).to be View::Books::Publish
         end
+
+        wrap_deferred 'when the provider defines components' do
+          context 'when the provider defines the matching component' do # rubocop:disable RSpec/NestedGroups
+            example_class 'Spec::Components::Views::Books::Publish',
+              ViewComponent::Base
+
+            it 'returns the component defined by the application' do
+              expect(component).to be View::Books::Publish
+            end
+          end
+        end
+      end
+    end
+
+    wrap_deferred 'when the provider defines components' do
+      it { expect(component).to be nil }
+
+      context 'when the provider defines the matching component' do
+        example_class 'Spec::Components::Views::Books::Publish',
+          ViewComponent::Base
+
+        it { expect(component).to be Spec::Components::Views::Books::Publish }
       end
     end
 
@@ -168,12 +213,16 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
           it { expect(component).to be View::Pages::Books::PublishPage }
         end
 
-        context 'when multiple sources are defined' do
-          example_class 'View::Books::Publish', ViewComponent::Base
-          example_class 'View::Pages::Books::PublishPage', ViewComponent::Base
+        wrap_deferred 'when the provider defines components' do
+          it { expect(component).to be nil }
 
-          it 'returns the component defined by the application' do
-            expect(component).to be View::Books::Publish
+          context 'when the provider defines the matching component' do # rubocop:disable RSpec/NestedGroups
+            example_class 'Spec::Components::Views::Books::Publish',
+              ViewComponent::Base
+
+            it 'should return the shared component' do
+              expect(component).to be Spec::Components::Views::Books::Publish
+            end
           end
         end
       end
@@ -205,6 +254,20 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
             ViewComponent::Base
 
           it { expect(component).to be View::Pages::Scoped::Books::PublishPage }
+        end
+      end
+
+      wrap_deferred 'when the provider defines components' do
+        it { expect(component).to be nil }
+
+        context 'when the provider defines the matching component' do
+          example_class 'Spec::Components::Views::Scoped::Books::Publish',
+            ViewComponent::Base
+
+          it 'should return the shared component' do
+            expect(component)
+              .to be Spec::Components::Views::Scoped::Books::Publish
+          end
         end
       end
     end
@@ -281,6 +344,20 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
           end
         end
       end
+
+      wrap_deferred 'when the provider defines components' do
+        it { expect(component).to be nil }
+
+        context 'when the provider defines the matching component' do
+          example_class 'Spec::Components::Views::Webhooks::Outbound::Send',
+            ViewComponent::Base
+
+          it 'should return the shared component' do
+            expect(component)
+              .to be Spec::Components::Views::Webhooks::Outbound::Send
+          end
+        end
+      end
     end
 
     context 'when a component class is cached' do
@@ -318,6 +395,16 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
     it { expect(service).to respond_to(:clear).with(0).arguments }
   end
 
+  describe '#components' do
+    include_examples 'should define reader',
+      :components,
+      Librum::Components::Empty
+
+    wrap_deferred 'when the provider defines components' do
+      it { expect(service.components).to be Spec::Components }
+    end
+  end
+
   describe '#libraries' do
     include_examples 'should define reader', :libraries, []
 
@@ -329,6 +416,256 @@ RSpec.describe Librum::Core::Responders::Html::FindView do
 
     wrap_deferred 'when the application includes libraries' do
       it { expect(service.libraries).to be == libraries }
+    end
+  end
+
+  describe '#view_paths' do
+    let(:action)     { 'publish' }
+    let(:controller) { 'books' }
+    let(:view_paths) { service.view_paths(action:, controller:) }
+    let(:legacy_path) do
+      'View::Pages::Books::PublishPage'
+    end
+    let(:expected) do
+      [legacy_path]
+    end
+
+    it 'should define the method' do
+      expect(service)
+        .to respond_to(:view_paths)
+        .with(0).arguments
+        .and_keywords(:action, :controller)
+    end
+
+    it { expect(view_paths).to be == expected }
+
+    # rubocop:disable RSpec/RepeatedExampleGroupBody
+    wrap_deferred 'when the application defines view paths' do
+      let(:application_path) do
+        'View::Books::Publish'
+      end
+      let(:expected) do
+        [
+          application_path,
+          legacy_path
+        ]
+      end
+
+      it { expect(view_paths).to be == expected }
+    end
+
+    wrap_deferred 'when the application namespace defines view paths' do
+      let(:application_path) do
+        'View::Books::Publish'
+      end
+      let(:expected) do
+        [
+          application_path,
+          legacy_path
+        ]
+      end
+
+      it { expect(view_paths).to be == expected }
+    end
+    # rubocop:enable RSpec/RepeatedExampleGroupBody
+
+    wrap_deferred 'when the provider defines components' do
+      let(:shared_path) do
+        'Spec::Components::Views::Books::Publish'
+      end
+      let(:expected) do
+        [
+          shared_path,
+          legacy_path
+        ]
+      end
+
+      it { expect(view_paths).to be == expected }
+    end
+
+    context 'when multiple sources are defined' do
+      let(:application_path) do
+        'View::Books::Publish'
+      end
+      let(:shared_path) do
+        'Spec::Components::Views::Books::Publish'
+      end
+      let(:expected) do
+        [
+          application_path,
+          shared_path,
+          legacy_path
+        ]
+      end
+
+      include_deferred 'when the application defines view paths'
+      include_deferred 'when the provider defines components'
+
+      it { expect(view_paths).to be == expected }
+    end
+
+    describe 'with controller: a scoped controller' do
+      let(:controller) { 'scoped/books' }
+      let(:legacy_path) do
+        'View::Pages::Scoped::Books::PublishPage'
+      end
+
+      it { expect(view_paths).to be == expected }
+
+      # rubocop:disable RSpec/RepeatedExampleGroupBody
+      wrap_deferred 'when the application defines view paths' do
+        let(:application_path) do
+          'View::Scoped::Books::Publish'
+        end
+        let(:expected) do
+          [
+            application_path,
+            legacy_path
+          ]
+        end
+
+        it { expect(view_paths).to be == expected }
+      end
+
+      wrap_deferred 'when the application namespace defines view paths' do
+        let(:application_path) do
+          'View::Scoped::Books::Publish'
+        end
+        let(:expected) do
+          [
+            application_path,
+            legacy_path
+          ]
+        end
+
+        it { expect(view_paths).to be == expected }
+      end
+      # rubocop:enable RSpec/RepeatedExampleGroupBody
+
+      wrap_deferred 'when the provider defines components' do
+        let(:shared_path) do
+          'Spec::Components::Views::Scoped::Books::Publish'
+        end
+        let(:expected) do
+          [
+            shared_path,
+            legacy_path
+          ]
+        end
+
+        it { expect(view_paths).to be == expected }
+      end
+
+      context 'when multiple sources are defined' do
+        let(:application_path) do
+          'View::Scoped::Books::Publish'
+        end
+        let(:shared_path) do
+          'Spec::Components::Views::Scoped::Books::Publish'
+        end
+        let(:expected) do
+          [
+            application_path,
+            shared_path,
+            legacy_path
+          ]
+        end
+
+        include_deferred 'when the application defines view paths'
+        include_deferred 'when the provider defines components'
+
+        it { expect(view_paths).to be == expected }
+      end
+    end
+
+    describe 'with controller: a controller belonging to a library' do
+      let(:action)     { 'send' }
+      let(:controller) { 'webhooks/outbound' }
+      let(:library_path) do
+        'WebhooksEngine::View::Outbound::Send'
+      end
+      let(:legacy_path) do
+        'Webhooks::View::Pages::Outbound::SendPage'
+      end
+      let(:expected) do
+        [
+          library_path,
+          legacy_path
+        ]
+      end
+
+      include_deferred 'when the application includes libraries'
+
+      it { expect(view_paths).to be == expected }
+
+      # rubocop:disable RSpec/RepeatedExampleGroupBody
+      wrap_deferred 'when the application defines view paths' do
+        let(:application_path) do
+          'View::Webhooks::Outbound::Send'
+        end
+        let(:expected) do
+          [
+            application_path,
+            library_path,
+            legacy_path
+          ]
+        end
+
+        it { expect(view_paths).to be == expected }
+      end
+
+      wrap_deferred 'when the application namespace defines view paths' do
+        let(:application_path) do
+          'View::Webhooks::Outbound::Send'
+        end
+        let(:expected) do
+          [
+            application_path,
+            library_path,
+            legacy_path
+          ]
+        end
+
+        it { expect(view_paths).to be == expected }
+      end
+      # rubocop:enable RSpec/RepeatedExampleGroupBody
+
+      wrap_deferred 'when the provider defines components' do
+        let(:shared_path) do
+          'Spec::Components::Views::Webhooks::Outbound::Send'
+        end
+        let(:expected) do
+          [
+            library_path,
+            shared_path,
+            legacy_path
+          ]
+        end
+
+        it { expect(view_paths).to be == expected }
+      end
+
+      context 'when multiple sources are defined' do
+        let(:application_path) do
+          'View::Webhooks::Outbound::Send'
+        end
+        let(:shared_path) do
+          'Spec::Components::Views::Webhooks::Outbound::Send'
+        end
+        let(:expected) do
+          [
+            application_path,
+            library_path,
+            shared_path,
+            legacy_path
+          ]
+        end
+
+        include_deferred 'when the application defines view paths'
+        include_deferred 'when the provider defines components'
+
+        it { expect(view_paths).to be == expected }
+      end
     end
   end
 end
