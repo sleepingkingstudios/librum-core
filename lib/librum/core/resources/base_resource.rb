e83d86bf3493f1f5c48a07db078bed1b4aceb0eb
Rob Smith <sleepingkingstudios@gmail.com>
2026-08-04 13:03:29 -0400

    Remove Librum::Core::Resources.

--- lib/librum/core/resources/base_resource.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'cuprum/rails/resource'
-
-module Librum::Core::Resources
-  # Abstract resource class for configuring Librum::Core controllers.
-  class BaseResource < Cuprum::Rails::Resource; end
-end
